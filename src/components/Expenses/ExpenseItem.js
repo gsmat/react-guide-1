c11dbde51e4921da946303dbe11d3ff2548632f1
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-06-25 16:32:03 +0400

    import first reactJs project

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import Card from '../UI/Card';
+import React from "react";
+import ExpenseDate from "../Expenses/ExpenseDate";
+import "../Expenses/ExpenseItem.css";
+
+const ExpenseItems = (props) => {
+  
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItems;
