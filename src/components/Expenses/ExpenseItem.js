c2b2919580170471e952be256c691c688cbbfca6
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-06-30 10:59:58 +0400

    delete unusefull code lines

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,17 @@
 import Card from "../UI/Card";
-import React , {useState} from "react";
+import React  from "react";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import "../Expenses/ExpenseItem.css";
 
 const ExpenseItems = (props) => {
-  const [title,setTitle] = useState(props.title);
-  const clickHandler = () => setTitle('Update Title');
   
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
