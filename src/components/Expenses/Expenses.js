aa54b600bff2530f42e5ae354cd81dd4a3314b86
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-06-30 11:00:47 +0400

    Adding dynamicly data from array  using map method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,24 @@
 import ExpenseItem from '../Expenses/ExpenseItem';
 import Card from '../UI/Card';
-import React from "react";
+import React , { useState}from "react";
 import '../Expenses/Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => setFilteredYear(selectedYear);
+ 
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {props.items.map(item => {
+        return (<ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />)
+      })}
     </Card>
   );
 }
